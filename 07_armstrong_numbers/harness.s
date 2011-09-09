40b27c7b12351e8bed23ce61819698bcef014e0a
Bob Wilkinson <bob@fourtheye.org>
2011-09-09 11:11:04 +0100

    itest power

---
+++ 07_armstrong_numbers/harness.s
@@ -0,0 +1,21 @@
+.globl main
+        .align 2
+.section .rodata
+value:
+	.byte 105
+evalue:
+	.byte 1
+
+        .align 2
+        .text
+main:
+	mov	r0, #3
+	mov	r1, #4
+#	mov	r0, #100
+#	sub	r0, r1, r2
+#	sub	r0, r2, r1
+#	bl	_armstrong
+	bl	_power
+
+	mov     r7, #1		@ set r7 to 1 - the syscall for exit
+	swi     0		@ then invoke the syscall from linux
