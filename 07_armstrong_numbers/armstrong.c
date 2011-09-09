b0a8c3917602d3e964b786477d99a80d762a0626
Bob Wilkinson <bob@fourtheye.org>
2011-09-09 23:58:44 +0100

    use quotes not angle brackets because i want to put them on a web page

---
+++ 07_armstrong_numbers/armstrong.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+#include "math.h"
+#include "stdlib.h"
+
+/* we allocate sufficient space to store the widest integer */
+#define MAXWIDTH 4
+
+/* numeric string characters are offset from their value */
+#define NUMOFFSET 48
+
+int main()
+{
+  int number;
+  for (number=10; number < 10000; number++)
+  {
+    char string[MAXWIDTH+1] = {};
+    snprintf(string, MAXWIDTH+1, "%d", number);
+    int numlen = strnlen(string, MAXWIDTH);
+     
+    int total = 0;
+    int j;
+    for (j=0; j < numlen; j++)
+    {
+      int digit = string[j] - NUMOFFSET;
+      total += pow(digit, numlen);
+    }
+    if (total == number)
+      printf("ARMSTRONG NUMBER is %d\n", total);
+  }
+  exit(0);
+}
