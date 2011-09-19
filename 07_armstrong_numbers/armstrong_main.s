bfb8200c7a638c1a7b221684b4b8ef8192494ab3
Bob Wilkinson <bob@fourtheye.org>
2011-09-19 17:20:01 +0100

    longer labels to avoid clashes

--- 07_armstrong_numbers/armstrong_main.s
+++ 07_armstrong_numbers/armstrong_main.s
@@ -14,23 +14,23 @@ main:
 	ldr	r6, =ten_thousand
 
 	mov	r4, r5		@ start with n = 10
-_loop:
+_main_loop:
 	cmp	r4, r6		@ leave if n = 10_000
-	beq	_end
+	beq	_main_end
 
 	mov	r0, r4		@ call the _armstrong function
 	bl	_armstrong
 
 	teq	r0, r4		@ if the armstong value = n print it
-	bne	_next		@ else skip
+	bne	_main_next		@ else skip
 
 	mov	r2, r0
 	mov	r1, r4
 	ldr	r0, =string	@ store address of start of string to r0
 	bl	printf		@ call the c function to display information
-_next:
+_main_next:
 	add	r4, r4, #1
-	b	_loop
-_end:
+	b	_main_loop
+_main_end:
 	mov	r7, #1		@ set r7 to 1 - the syscall for exit
 	swi	0		@ then invoke the syscall from linux
